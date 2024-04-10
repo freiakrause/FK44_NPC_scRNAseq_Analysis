7bbadd1cd432c1b9515b357e186e75e12882b755
Freia Krause <fkrause@brenda.came.sbg.ac.at>
2024-04-10 11:28:49 +0200

    Initial Commit from Brenda

---
+++ FK44_1_COUNT_Script.R
@@ -0,0 +1,11 @@
+#This is the script to analyse FK44.1 scRNAseq COUNT data provided by BSF
+#install.packages("renv")
+renv::init()    #creates project library,  lockfile and Rprofile
+#renv::snapshot() # updates lockfile abou current-used packages-> can be shared and reproduced by others when restore() is used
+#renv::restore() # installs the exact same versiosn of packages determined in lockfile
+#renv::update()) #
+#renv::history() #
+#renv::install("usethis")
+#usethis::create_github_token()
+#gitcreds::gitcreds_set()
+
