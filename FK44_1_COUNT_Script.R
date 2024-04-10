fc3476a87962807f54c3c5cf9cad79c69173cc0d
freiakrause_workPC <freia.krause@plus.ac.at>
2024-04-10 13:51:37 +0200

    Work PC to GitHub

--- FK44_1_COUNT_Script.R
+++ FK44_1_COUNT_Script.R
@@ -1,13 +1,12 @@
 #This is the script to analyse FK44.1 scRNAseq COUNT data provided by BSF
-#install.packages("renv")
-#renv::init()    #creates project library,  lockfile and Rprofile
+install.packages("renv")
+renv::init()    #creates project library,  lockfile and Rprofile
 #renv::snapshot() # updates lockfile abou current-used packages-> can be shared and reproduced by others when restore() is used
 #renv::restore() # installs the exact same versiosn of packages determined in lockfile
 #renv::update()) #
 #renv::history() #
-#renv::install("usethis")
-#usethis::create_github_token()
-#gitcreds::gitcreds_set()
-Hallo
-type stuff on work PC
+renv::install("usethis")
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
 
